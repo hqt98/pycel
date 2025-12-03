89da61006af772ff025c1cef773a142178600d1b
hqt98 <hqt98@outlook.com>
2025-12-03 15:20:14 +0700

    Update deps

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ to install a link for development work:
 from setuptools import find_packages, setup
 
 # see StackOverflow/458550
-exec(open('src/pycel/version.py').read())
+exec(open("src/pycel/version.py").read())
 
 
 # Create long description from README.rst and CHANGES.rst.
@@ -32,59 +32,60 @@ def changes():
     import itertools as it
     import re
 
-    lines = tuple(open('CHANGES.rst', 'r', encoding='utf-8').readlines())
-    first_change_re = re.compile(r'^\[\d')
+    lines = tuple(open("CHANGES.rst", "r", encoding="utf-8").readlines())
+    first_change_re = re.compile(r"^\[\d")
     header = tuple(it.takewhile(lambda line: not first_change_re.match(line), lines))
-    return lines[len(header):]
+    return lines[len(header) :]
 
 
-long_description = u'{}\n\nChange Log\n==========\n\n{}'.format(
-    open('README.rst', 'r', encoding='utf-8').read(), ''.join(changes()))
+long_description = "{}\n\nChange Log\n==========\n\n{}".format(
+    open("README.rst", "r", encoding="utf-8").read(), "".join(changes())
+)
 
-with open('test-requirements.txt') as f:
+with open("test-requirements.txt") as f:
     tests_require = f.readlines()
 
 
 setup(
-    name='pycel',
+    name="pycel",
     version=__version__,  # noqa: F821
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    description='A library for compiling excel spreadsheets to python code '
-                '& visualizing them as a graph',
-    keywords='excel compiler formula parser',
-    url='https://github.com/stephenrauch/pycel',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    description="A library for compiling excel spreadsheets to python code "
+    "& visualizing them as a graph",
+    keywords="excel compiler formula parser",
+    url="https://github.com/stephenrauch/pycel",
     project_urls={
         # 'Documentation': 'https://pycel.readthedocs.io/en/stable/',
-        'Tracker': 'https://github.com/stephenrauch/pycel/issues',
+        "Tracker": "https://github.com/stephenrauch/pycel/issues",
     },
     tests_require=tests_require,
-    test_suite='pytest',
+    test_suite="pytest",
     install_requires=[
-        'networkx>=2.0,<2.7',
-        'numpy',
-        'openpyxl>=2.6.2',
-        'python-dateutil',
-        'ruamel.yaml',
+        "networkx>=3.0",
+        "numpy",
+        "openpyxl>=2.6.2",
+        "python-dateutil",
+        "ruamel.yaml",
     ],
-    python_requires='>=3.6',
-    author='Dirk Gorissen, Stephen Rauch',
-    author_email='dgorissen@gmail.com',
-    maintainer='Stephen Rauch',
-    maintainer_email='stephen.rauch+pycel@gmail.com',
+    python_requires=">=3.6",
+    author="Dirk Gorissen, Stephen Rauch",
+    author_email="dgorissen@gmail.com",
+    maintainer="Stephen Rauch",
+    maintainer_email="stephen.rauch+pycel@gmail.com",
     long_description=long_description,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
